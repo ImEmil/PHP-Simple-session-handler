88d80341eff9a85956f9b3fb2f1f86cc0c76fde6
ImEmil <emil_nilsson_96@live.se>
2013-12-09 20:32:40 +0100

    Update example.php

--- example.php
+++ example.php
@@ -9,12 +9,20 @@
 </head>
 
 <body>
-  <!-- Form shows if the session isn't created yet -->
+  <?php if($s->isLoggedIn())
+  {
+    echo "Welcome beloved user, you are currently logged in!";
+  }
+  else {
+    ?>
+    <!-- Form shows if the session isn't created yet -->
   <form method="post" action="">
     <label for="username">
     <input type="text" name="username" id="username" placeholder="Type in your username">
     <input type="submit" value="Continue &raquo;">
   </form>
-  
+  <?php
+  }
+  ?>
 </body>
 </html>
