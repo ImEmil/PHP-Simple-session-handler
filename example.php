ddb30c2db2f0d0171f010407e1b198a5d67e1535
ImEmil <emil_nilsson_96@live.se>
2013-12-09 20:30:19 +0100

    Create example.php

---
+++ example.php
@@ -0,0 +1,20 @@
+<?php
+  require("engine.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Example</title>
+</head>
+
+<body>
+  <!-- Form shows if the session isn't created yet -->
+  <form method="post" action="">
+    <label for="username">
+    <input type="text" name="username" id="username" placeholder="Type in your username">
+    <input type="submit" value="Continue &raquo;">
+  </form>
+  
+</body>
+</html>
