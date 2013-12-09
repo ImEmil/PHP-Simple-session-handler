14e75a88b06ad347322b40765cbf365400375dda
ImEmil <emil_nilsson_96@live.se>
2013-12-09 20:21:18 +0100

    Update class.session_handler.php

--- class.session_handler.php
+++ class.session_handler.php
@@ -1,26 +1,29 @@
 <?php
 class Sessions{
-public function redirect($url, $seconds) {
-	echo '<meta http-equiv="refresh" content="'.$seconds.';url='.$url.'">';
-}
-	public function login_session () {
-		return(isset($_SESSION['username'])) ? true : false;
+	
+	public function redirect($url, $seconds) {
+		echo '<meta http-equiv="refresh" content="'.$seconds.';url='.$url.'">';
 	}
-
+	
+	public function c_session () {
+		return(isset($_SESSION['logged'])) ? true : false;
+	}
+	
 	public function isLoggedIn() {
-		if ($this->login_session() === true) {
+		if ($this->c_session() === true) {
 			return true;
 			}
 			else {
 				return false;
+			}
 		}
-	}
 	 
-	public function isLoggedOut() {
-		if ($this->login_session() === false) {
+	public function isLoggedIn() {
+		if ($this->c_session() === false) {
 			return true;
+			}
+			else {
+				return false;
+			}
 		}
-		else {
-			return false;
-		}
-	}
+?>
