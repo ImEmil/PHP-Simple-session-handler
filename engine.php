63585eb294da5f96f2a3112e7b8e5f2ce49789da
ImEmil <emil_nilsson_96@live.se>
2013-12-09 21:10:27 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -1,5 +1,5 @@
 <?php
   session_start(); // Starts the session so we can use them further
-  require('classes/class.session_handler.php');
+  require('class.session_handler.php');
   $s  = new Sessions(); // Initiates the class
 ?>
