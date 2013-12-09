7fd4d34dfd0a7487925db1c8a572a0405007d04e
ImEmil <emil_nilsson_96@live.se>
2013-12-09 20:38:56 +0100

    Update example.php

--- example.php
+++ example.php
@@ -9,20 +9,29 @@
 </head>
 
 <body>
+
   <?php if($s->isLoggedIn())
   {
-    echo "Welcome beloved user, you are currently logged in!";
+    echo "Welcome {$_SESSION['username'])}, you are currently logged in!";
   }
   else {
     ?>
-    <!-- Form shows if the session isn't created yet -->
+    
+<!-- Form shows if the session isn't created yet -->
   <form method="post" action="">
     <label for="username">
     <input type="text" name="username" id="username" placeholder="Type in your username">
-    <input type="submit" value="Continue &raquo;">
+    <input type="submit" name="session" value="Continue &raquo;">
   </form>
+  
   <?php
+  if(isset($_POST['session'])) {
+    $_SESSION["logged"] = true; // Now the session is true so the handler will know that we are logged in
+    $_SESSION["username"] = trim(htmlspecialchars($_POST["username"])); // This is just to show how you could use the session handler
+    header("Location example.php"); // Refresh the page
+    }
   }
   ?>
+  
 </body>
 </html>
